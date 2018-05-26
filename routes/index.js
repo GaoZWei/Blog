0a6b3d6c09398b225c19327a9449954f9d1913cf
Gao <1520045270@qq.com>
2018-05-26 15:05:44 +0800

    实现页面的跳转

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
+/* GET admin page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Express' });
 });
+
+/* GET persoanl page. */
+router.get('/personal', function(req, res, next) {
+    res.render('personal/personal', { title: 'Express' });
+});
+
+router.get('/front_list',function (req,res,next) {
+    res.render('front/front_list');
+});
+router.get('/front_content',function (req,res,next) {
+    res.render('front/front_content');
+})
+router.get('/hobby',function (req,res,next) {
+    res.render('hobby/hobby');
+})
+router.get('/problem_list',function (req,res,next) {
+    res.render('problem/problem_list');
+})
+router.get('/problem_content',function (req,res,next) {
+    res.render('problem/problem_content');
+})
+
 
 module.exports = router;
