1c6d7ed6468d4106176f3b2acdf9d69e6bd08268
Chipmunkkk <1033780702@qq.com>
2018-05-27 16:37:09 +0800

    2018-05-27-16:36 更新部分首页后台,重写与博客相关的SQL语句

--- dao/blogDao.js
+++ dao/blogDao.js
@@ -1,4 +1,5 @@
-var pool = require('../db/dbConfig');
+var pool = require('../db/db');
+var moment = require('moment');
 
 var PER_PAGE = 10; //每页显示的数量
 var NOW_DATE = new Date(); //当前时间
@@ -7,26 +8,44 @@ var NOW_DATE = new Date(); //当前时间
  * 获取所有博客
  *
  * @param params 浏览器参数
- * @param cb 回调函数
  */
-function getBlogs(params, cb) {
-    var archive = params.archive;
-    var tag = params.tag;
-    var page = params.page;
-    var sql = 'select * from Article where id is not null ';
-    if (archive != null) {
-        sql += 'and archive = "测试分类" '
-    }
-    if (tag != null) {
-        sql += 'and tag = "测试标签" '
-    }
-    if (page == null) {
-        page = 1;
-    }
-    sql += 'limit ' + (page - 1) * PER_PAGE + ',' + PER_PAGE;
-    console.log('sql: ' + sql);
-    pool.query(sql, function (error, results) {
-        cb(error, results);
+function getBlogs(params) {
+    return new Promise(function (resolve, reject) {
+        var archive = params.archive;
+        var tag = params.tag;
+        var page = params.page;
+
+        var sql = 'SELECT a.* ,GROUP_CONCAT(t.tagName SEPARATOR ",") as tags ' +
+            ',GROUP_CONCAT(DISTINCT ach.archiveName SEPARATOR ",") as archive ' +
+            'from Article a left join Tag t on t.articleId = a.id ' +
+            'left join Archive ach on ach.articleId = a.id GROUP BY a.id';
+        var sql1 = 'SELECT * from (' + sql + ') tmp where tags like "%' + tag + '%"';
+        var sql2 = 'SELECT * from (' + sql + ') tmp where archive like "%' + archive + '%"';
+
+        if (archive != null) {
+            sql = sql2;
+        }
+        if (tag != null) {
+            sql = sql1;
+        }
+        if (page == null) {
+            page = 1;
+        }
+
+        sql += ' limit ' + (page - 1) * PER_PAGE + ',' + PER_PAGE;
+        console.log('sql: ' + sql);
+        pool.query(sql, function (error, results) {
+            if (!error) {
+                for (var i = 0; i < results.length; i++) {
+                    results[i].updateTime = moment(results[i].updateTime).format('YYYY年MM月DD日');
+                    results[i].tags = results[i].tags.toString().split(',');
+                }
+                console.log(results);
+                resolve(results);
+            } else {
+                reject(error);
+            }
+        });
     });
 }
 
@@ -34,12 +53,17 @@ function getBlogs(params, cb) {
  * 获取指定Id的博客
  *
  * @param id 博客Id
- * @param cb 回调函数
  */
-function getBlog(id, cb) {
-    var sql = 'select * from Article where id=?';
-    pool.query(sql, [id], function (error, results) {
-        cb(error, results);
+function getBlog(id) {
+    return new Promise(function (resolve, reject) {
+        var sql = 'select * from Article where id=?';
+        pool.query(sql, [id], function (error, results) {
+            if (!error) {
+                resolve(results);
+            } else {
+                reject(error);
+            }
+        })
     })
 }
 
@@ -47,16 +71,40 @@ function getBlog(id, cb) {
  * 创建博客
  *
  * @param params 浏览器参数
- * @param cb 回调函数
  */
-function createBlog(params, cb) {
-    var sql1 = 'select max(id) as maxId from Article';
-    var sql2 = 'insert into Article values(?,?,?,?,?,?)';
-    pool.query(sql1, function (error, results) {
-        pool.query(sql2, [results[0].maxId + 1, params.title, params.archive, params.tag, NOW_DATE, params.content],
-            function (error, results, fileds) {
-                cb(error, results);
-            })
+function createBlog(params) {
+    return new Promise(function (resolve, reject) {
+        var sql1 = 'select max(id) as maxId from Article';
+        var sql2 = 'insert into Article values(?,?,?,?,?)';
+        var sql3 = 'insert into Archive values(?,?)';
+        var sql4 = 'insert into Tag values(?,?)';
+        pool.query(sql1, function (error, results) {
+            var id = results[0].maxId + 1;
+            pool.query(sql2, [id, params.title, NOW_DATE, params.description, params.content],
+                function (error, results) {
+                    pool.query(sql3, [params.archive, id], function (error, results) {
+                        if (typeof params.tag === 'object') {
+                            for (var i = 0; i < params.tag.length; i++) {
+                                pool.query(sql4, [params.tag[i], id], function (error, results) {
+                                    if (!error) {
+                                        resolve(results);
+                                    } else {
+                                        reject(error);
+                                    }
+                                })
+                            }
+                        } else {
+                            pool.query(sql4, [params.tag, id], function (error, results) {
+                                if (!error) {
+                                    resolve(results);
+                                } else {
+                                    reject(error);
+                                }
+                            })
+                        }
+                    })
+                })
+        })
     })
 }
 
@@ -65,27 +113,64 @@ function createBlog(params, cb) {
  *
  * @param params 浏览器参数
  * @param id 博客Id
- * @param cb 回调函数
  */
-function updateBlog(params, id, cb) {
-    var sql = 'update Article set title=?,archive=?,tag=?,updateTime=?,content=? where id=?';
-    pool.query(sql, [params.title, params.archive, params.tag, NOW_DATE, params.content, id],
-        function (error, reuslts) {
-            cb(error, reuslts);
-        })
+function updateBlog(params, id) {
+    return new Promise(function (resolve, reject) {
+        var archive = params.archive;
+        var tag = params.tag;
+        var sql = 'update Article set title=?,updateTime=?,description=?,content=? where id=?;';
+        var sql1 = 'delete from Archive where articleId=?;';
+        var sql2 = 'insert into Archive values(?,?);';
+        var sql3 = 'delete from Tag where articleId=?;';
+        var sql4 = 'insert into Tag values(?,?);';
+        pool.query(sql, [params.title, NOW_DATE, params.description, params.content, id],
+            function (error, reuslts) {
+                pool.query(sql1 + sql2 + sql3, [id, archive, id, id], function (error, result) {
+                    if (typeof tag === 'object') {
+                        for (var i = 0; i < tag.length; i++) {
+                            pool.query(sql4, [tag[i], id], function (error, results) {
+                                if (!error) {
+                                    resolve(results);
+                                } else {
+                                    reject(error);
+                                }
+                            })
+                        }
+                    } else {
+                        pool.query(sql4, [tag, id], function (error, results) {
+                            if (!error) {
+                                resolve(results);
+                            } else {
+                                reject(error);
+                            }
+                        })
+                    }
+                });
+
+            })
+    })
+
 }
 
 /**
  * 删除指定Id的博客
  *
  * @param id 博客Id
- * @param cb 回调函数
  */
-function deleteBlog(id, cb) {
-    var sql = 'delete from Article where id=?';
-    pool.query(sql, [id], function (error, results) {
-        cb(error, results);
+function deleteBlog(id) {
+    return new Promise(function (resolve, reject) {
+        var sql = 'delete from Article where id=?;';
+        var sql1 = 'delete from Archive where articleId=?;';
+        var sql2 = 'delete from Tag where articleId=?;';
+        pool.query(sql + sql1 + sql2, [id, id, id], function (error, results) {
+            if (!error) {
+                resolve(results);
+            } else {
+                reject(error);
+            }
+        })
     })
+
 }
 
 module.exports.getBlogs = getBlogs;
